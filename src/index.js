6008bf56c4731748255604139c63d3dac88a7f1e
duongtdn <duongtdn@gmail.com>
2019-10-10 08:04:47 +0000

    Add request method, get and post is the wrapper of request. Increase version

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@
 const storage = require('./storage')
 
 module.exports = {
+
   get(url, options) {
+    return this.request({method: 'GET', url, options})
+  },
+
+  post(url, data, options) {
+    if (options) {
+      if (!options.header) { options.header = {} }
+      options.header['Content-Type'] = 'application/json'
+    } else {
+      options = { header: { 'Content-Type' : 'application/json' } }
+    }
+    return this.request({ method: 'POST', url, data, options })
+  },
+
+  request({method, url, data, header, options}) {
     return new Promise( (resolve, reject) => {
       const request = new XMLHttpRequest()
-      request.open('GET', url, true)
+      request.open(method, url, true)
+      if (options && options.header) {
+        for (let prop in options.header) {
+          request.setRequestHeader(prop, options.header[prop])
+        }
+      }
       if (options && options.authen && storage.get('token')) {
         request.setRequestHeader('Authorization', `Bearer ${storage.get('token')}`)
       }
@@ -15,7 +35,12 @@ module.exports = {
         to && clearTimeout(to)
         resolve(request.status, request.responseText)
       })
-      request.send()
+      if (data) {
+        request.send(JSON.stringify(data))
+      } else {
+        request.send()
+      }
     })
   }
+
 }
